c96c1c90350658f4e5d4d4718f605282ac41aa9d
unknown <nikola.bogo27@gmail.com>
2024-11-21 16:33:44 +0100

    finished that bitch

---
+++ 2d-project-start/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+signal health_depleted
+
+@onready var happy_boo: Node2D = %HappyBoo
+
+var health = 100.0
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		happy_boo.play_walk_animation()
+	else:
+		happy_boo.play_idle_animation()
+
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
+	
